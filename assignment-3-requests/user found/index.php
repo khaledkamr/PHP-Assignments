07ec7ae061516296462910a1834c609e773527b4
khaledkamr <200013171@must.edu.eg>
2024-11-18 14:03:24 +0200

    fixed form

--- assignment-3-requests/user found/index.php
+++ assignment-3-requests/user found/index.php
@@ -13,8 +13,6 @@
         </style>
     </head>
     <body>
-
-    <div class="container">
         <?php
             $users = [
                 ["username" => "khaled", "password" => "123"],
@@ -46,27 +44,31 @@
                 }
             }
         ?>
-        
-        <?php if (isset($_POST["submit"])): ?>
-            <div class="alert alert-danger"><?php echo $message; ?></div>
-        <?php endif; ?>
 
-        <form method="post" action="" class="form-group">
-            <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="text" name="username" class="form-control">
+        <div class="container">
+            <?php if ($message != ""): ?>
+                <div class="alert alert-danger"><?php echo $message; ?></div>
+            <?php endif; ?>
+            
+            <div class="row">
+                <div class="col-md-12 p-4 shadow-sm rounded border">
+                    <form method="post" action="" class="form-group">
+                        <div class="form-group">
+                            <label for="username">Username:</label>
+                            <input type="text" name="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary btn-block">Login</button>
+                    </form>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" name="password" class="form-control">
-            </div>
-            <button type="submit" name="submit" class="btn btn-primary btn-block">Login</button>
-        </form>
-    </div>
+        </div>
 
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </body>
 </html>
