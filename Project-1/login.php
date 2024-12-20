48f70507a9ed0e77eb3849bfb24f074766ac68f4
Khaled Kamr <200013171@must.edu.eg>
2024-12-20 18:21:29 +0200

    Authentication

--- Project-1/login.php
+++ Project-1/login.php
@@ -6,20 +6,27 @@ include "dbConnection.php";
 
 <div class="card-body px-5 py-5" style="background-color:darkgray;">
 
-
+<?php if(isset($_SESSION["success"])): ?>
+<div class="alert alert-info" role="alert">
+  <?php echo $_SESSION["success"] ?>
+</div>
+<?php endif ?>
 
             
               
                 <h3 class="card-title text-left mb-3">Login</h3>
-                <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+                <form action="loginHandle.php" method="post">
+
                   <div class="form-group">
                     <label>email *</label>
                     <input type="email" name="email" class="form-control p_input" >
                   </div>
+
                   <div class="form-group">
                     <label>Password *</label>
                     <input type="text" name="password" class="form-control p_input" >
                   </div>
+
                   <div class="form-group d-flex align-items-center justify-content-between">
                     <div class="form-check">
                       <label class="form-check-label">
@@ -27,9 +34,11 @@ include "dbConnection.php";
                     </div>
                     <a href="forgetPassword.php" class="forgot-pass">Forgot password</a>
                   </div>
+
                   <div class="text-center">
                     <button type="submit" name="login" class="btn btn-primary btn-block enter-btn">Login</button>
                   </div>
+                  
                   <div class="d-flex">
                     <button class="btn btn-facebook me-2 col">
                       <i class="mdi mdi-facebook"></i> Facebook </button>
@@ -48,7 +57,10 @@ include "dbConnection.php";
       <!-- page-body-wrapper ends -->
     </div>
 
-    <?php include "footer.php" ?>
+    <?php 
+    include "footer.php" ;
+    unset($_SESSION["success"]);
+    ?>
 
 
     //table user, product, cart ,, review comment , rating  = session
--- Project-1/signup.php
+++ Project-1/signup.php
@@ -3,20 +3,34 @@
 include "header.php";
 include "navbar.php";
 include "dbConnection.php";
+
+$username = isset($_SESSION["username"]) ? $_SESSION["username"] : "";
+$email = isset($_SESSION["email"]) ? $_SESSION["email"] : "";
+$phone = isset($_SESSION["phone"]) ? $_SESSION["phone"] : "";
+$address = isset($_SESSION["address"]) ? $_SESSION["address"] : "";
 ?>
 
 <div class="card-body px-5 py-5" style="background-color:darkgray;">
-
+                
+<?php 
+  if(isset($_SESSION["errors"])) {
+    foreach($_SESSION["errors"] as $error): ?>
+      <div class="alert alert-danger container"><?php echo $error; ?></div>
+<?php 
+    endforeach;
+  }
+?>
                 <h3 class="card-title text-left mb-3">Register</h3>
-                <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+                <form action="insert.php" method="post">
+
                   <div class="form-group">
                     <label>Username</label>
-                    <input type="text" class="form-control p_input" name="UserName" value="">
+                    <input type="text" class="form-control p_input" name="username" value="<?php echo $username; ?>">
                   </div>
                   
                   <div class="form-group">
                     <label>Email</label>
-                    <input type="email" class="form-control p_input" name="email">
+                    <input type="email" class="form-control p_input" name="email" value="<?php echo $email; ?>">
                   </div>
 
                   <div class="form-group">
@@ -26,12 +40,12 @@ include "dbConnection.php";
 
                   <div class="form-group">
                     <label>Phone</label>
-                    <input type="text" class="form-control p_input"name="phone">
+                    <input type="text" class="form-control p_input"name="phone" value="<?php echo $phone; ?>">
                   </div>
 
                   <div class="form-group">
                     <label>Address</label>
-                    <input type="text" class="form-control p_input" name="address">
+                    <input type="text" class="form-control p_input" name="address" value="<?php echo $address; ?>">
                   </div>
               
                   <div class="form-group d-flex align-items-center justify-content-between">
@@ -40,6 +54,7 @@ include "dbConnection.php";
                   <div class="text-center">
                     <button type="submit" name="signup" class="btn btn-primary btn-block enter-btn">Signup</button>
                   </div>
+
                   <div class="d-flex">
                     <button class="btn btn-facebook col me-2">
                       <i class="mdi mdi-facebook"></i> Facebook </button>
@@ -69,3 +84,10 @@ include "dbConnection.php";
   preg_match($regex,) 
   
   -->
+<?php 
+unset($_SESSION["errors"]);
+unset($_SESSION["username"]);
+unset($_SESSION["email"]);
+unset($_SESSION["phone"]);
+unset($_SESSION["address"]);
+?>
