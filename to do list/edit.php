f8c413ef86aee0c22fcf15418dd267d2b6402b7e
Khaled Kamr <200013171@must.edu.eg>
2025-01-28 16:06:50 +0200

    flexibilty to update the note

--- to do list/edit.php
+++ to do list/edit.php
@@ -1,10 +1,35 @@
 <?php
 require_once 'inc/header.php';
+require_once "App.php";
+require_once "inc/connection.php";
+
+if($req->check($req->get("id"))) {
+    $id = $req->get("id");
+}
+
+$res = $conn->prepare("SELECT `title` FROM todo WHERE id=:id");
+$res->bindParam(":id", $id, PDO::PARAM_INT);
+$res->execute();
+$title = $res->fetch(PDO::FETCH_ASSOC);
+
+if(!count($title) > 0) {
+    $req->redirect("index.php");
+}
 ?>
 
 <body class="container w-50 mt-5">
-    <form action="handle/edit.php?id=" method="post">
-            <textarea type="text" class="form-control"  name="title" id="" placeholder="enter your note here"></textarea>
+    <?php
+        if($ses->get("errors")) { 
+            foreach($ses->get("errors") as $error) {
+            ?>
+            <div class="alert alert-danger"><?php echo $error ?></div>
+        <?php
+            }
+        }
+        $ses->remove("errors");
+    ?>    
+    <form action="handle/edit.php?id=<?php echo $id ?>" method="post">
+            <textarea type="text" class="form-control"  name="title" id="" placeholder="enter your note here"><?php echo $title['title'] ?></textarea>
             <div class="text-center">
                 <button type="submit" name="submit" class="form-control text-white bg-info mt-3 " >Update</button>
             </div>  
--- to do list/handle/edit.php
+++ to do list/handle/edit.php
@@ -1,1 +1,44 @@
 <?php
+require_once "../inc/connection.php";
+require_once "../App.php";
+
+if($req->check($req->post("submit")) && $req->check($req->get("id"))) {
+    $id = $req->get("id");
+    $title = $req->filter($req->post("title"));
+    $val->validate("title", $title, ["Required", "Str"]);
+    $errors = $val->getError();
+
+    if($errors == false) {
+        $res = $conn->prepare("SELECT `title` FROM todo WHERE id=:id");
+        $res->bindParam(":id", $id, PDO::PARAM_INT);
+        $res->execute();
+        $out = $res->fetch(PDO::FETCH_ASSOC);
+
+        if(count($out) > 0) {
+            $res = $conn->prepare("UPDATE todo SET title=:title WHERE id=:id");
+            $res->bindParam(":title", $title, PDO::PARAM_STR);
+            $res->bindParam(":id", $id, PDO::PARAM_INT);
+            $out = $res->execute();
+
+            if($out) {
+                $ses->set("success", "note updated successfully");
+                $req->redirect("../index.php");
+            }
+            else {
+                $ses->set("errors", ["error while update"]);
+                $req->redirect(".../edit.php?id=$id");
+            }
+        }
+        else {
+            $ses->set("errors", ["title not found"]);
+            $req->redirect("../index.php");
+        }
+    }
+    else {
+        $ses->set("errors", $errors);
+        $req->redirect("../edit.php?id=$id");
+    }
+}
+else {
+    $req->redirect("../index.php");
+}
