b3acedfecc9c0ac0501740bb25cef47651324250
Khaled Kamr <200013171@must.edu.eg>
2025-02-26 17:34:01 +0200

    redirect user and admin

---
+++ e-commerce/app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function redirect() {
+        if(Auth::user()->role == 'admin') {
+            return view('admin.home');
+        }
+        else {
+            return view('user.home');
+        }
+    }
+}
