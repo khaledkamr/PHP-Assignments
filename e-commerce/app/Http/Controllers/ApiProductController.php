ec025c2645d4a9be5d8b8102c2048d849d9ae446
Khaled Kamr <200013171@must.edu.eg>
2025-02-28 17:27:18 +0200

    Added api for update and delete product

--- e-commerce/app/Http/Controllers/ApiProductController.php
+++ e-commerce/app/Http/Controllers/ApiProductController.php
@@ -55,4 +55,66 @@ class ApiProductController extends Controller
             "msg" => "product created successfully!"
         ], 201);
     }
+
+    public function update($id, Request $request) {
+        $product = Product::find($id);
+        if($product == null) {
+            return response()->json([
+                "msg" => "data not found"
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'desc' => 'required|string',
+            'price' => 'required|numeric',
+            'image' => 'image|mimes:jpg,png,jpeg',
+            'quantity' => 'required|numeric'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                "error" => $validator->errors()
+            ], 301);
+        }
+
+        $image = $product->image;
+
+        if($request->hasFile('image')) {
+            Storage::delete($product->image);
+            $image = Storage::putFile("products", $request->image);
+        }
+
+        $product->update([
+            "name" => $request->name,
+            "desc" => $request->desc,
+            "price" => $request->price,
+            "quantity" => $request->quantity,
+            "image" => $image
+        ]);
+
+        return response()->json([
+            "msg" => "product updated successfully!",
+            "product" => new ProductResource(($product))
+        ], 201);
+    }
+
+    public function delete($id) {
+        $product = Product::find($id);
+        if($product == null) {
+            return response()->json([
+                "msg" => "data not found"
+            ], 404);
+        }
+
+        if($product->image !== null) {
+            Storage::delete($product->image);
+        }
+
+        $product->delete();
+
+        return response()->json([
+            "msg" => "product deleted successfully!"
+        ], 201);
+    }
 }
